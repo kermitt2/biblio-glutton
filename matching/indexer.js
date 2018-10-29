760a82691a77d91969ae36d66e9e0fe7a7dbaa40
lfoppiano <luca@foppiano.org>
2018-10-29 23:45:54 +0100

    fixing async stuff

--- matching/indexer.js
+++ matching/indexer.js
@@ -18,90 +18,120 @@ let client = new elasticsearch.Client({
     // suggestCompression: true
 });
 
-function print(body) {
-    process.stdout.write(body + "\n");
-}
-
-//Test connection
-client.ping({
-    requestTimeout: 30000,
-}).then(function (body) {
-    print("Cluster alive");
-}, function (error) {
-    print("Cannot contact cluster: " + error);
-});
-
-let analyserFile = "resources/analyzer.json";
-let mappingFile = "resources/crossref_mapping.json";
+// let analyserPath = "./resources/analyzer.json";
+let analyserPath = "/Users/lfoppiano/development/scienceminer/biblio-glutton/matching/resources/analyzer.json";
+let mappingPath = "./resources/crossref_mapping.json";
 
 let input = '/Volumes/SEAGATE1TB/scienceminer/crossref/crossref-works.2018-09-05.json.xz';
 
-client.indices
-    .delete(
-        {
-            index: "crossref",
-            ignore: [404]
-        },
-        function (error, response) {
-            if (error) {
-                print("Error deleting index: " + error)
+function createIndex(index) {
+    var analyserFile = fs.readFileSync(analyserPath, 'utf-8');
+    var mappingFile = fs.readFileSync(mappingPath, 'utf-8').toString();
+
+    client.indices
+        .create(
+            {
+                index: index,
+                ignore: [404, 400],
+                mapping: mappingFile
+            },
+            function (err, resp) {
+                if (err)
+                    console.log('Failed to create ElasticSearch index, ' + err.message);
+                else {
+                    console.log('Successfully created ElasticSearch index');
+                    // client.indexes.putMapping(mappingFile)
+                }
             }
+        );
+}
+
+function delete_index(index) {
+    client.indices
+        .delete(
+            {
+                index: index,
+                ignore: [404]
+            },
+            function (error, response) {
+                if (error) {
+                    console.log("Error deleting index: " + error)
+                }
+            }
+        );
+}
+
+async function init_cluster(index) {
+    try {
+        await client.ping({
+            requestTimeout: 30000,
+        }).then(function (body) {
+            console.log("Cluster alive");
+        }, function (error) {
+            console.log("Cannot contact cluster: " + error);
+            process.exit(-1);
+        });
+
+        await delete_index(index);
+        await createIndex(index);
+        console.log('Init done.')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+init_cluster("crossref")
+    .then(body => {
+            console.log("Loading file " + input);
+            load_file(input);
         }
     );
 
-client.indices
-    .create(
-        {
-            index: 'crossref',
-            ignore: [404, 400]
-        },
-        function (err, resp) {
-            if (err)
-                print('Failed to create ElasticSearch index, ' + err.message);
-            else
-                print('Successfully created ElasticSearch index');
-        }
-    );
 
+function load_file(input) {
+    fs.createReadStream(input)
+        .pipe(lzma.createDecompressor())
+        .pipe(es.split())
+        .pipe(es.map(function (data, cb) {
+            // prepare/massage the data
 
-fs.createReadStream(input)
-    .pipe(lzma.createDecompressor())
-    .pipe(es.split())
-    .pipe(es.map(function (data, cb) {
-        // prepare/massage the data
+            // - migrate id from '_id' to 'id'
+            var obj = JSON.parse(data);
+            obj.id = obj._id.$oid;
+            delete obj._id;
 
-        // - migrate id from '_id' to 'id'
-        var obj = JSON.parse(data);
-        obj.id = obj._id.$oid;
-        delete obj._id;
+            // - remove citation data
+            delete obj.reference;
 
-        // - remove citation data
-        delete obj.reference;
+            cb(null, obj)
+        }))
+        .pipe(es.map(function (line, cb) {
 
-        cb(null, obj)
-    }))
-    .pipe(es.map(function (line, cb) {
+            var response = undefined;
+            try {
 
-        var response = undefined;
-        try {
+                response = client.index({
+                    index: 'crossref',
+                    type: 'item',
+                    body: line
+                });
 
-            response = client.index({
-                index: 'crossref',
-                type: 'item',
-                body: line
-            });
-
-        } catch (error) {
-            console.trace(error)
-        }
-        cb(null, response)
-    }))
-    .on('error', function (error) {
-        process.stderr.write("Error occurred: " + error);
-    })
-    .on('finish', function () {
-        process.stdout.write("Finished. ")
-    });
+            } catch (error) {
+                console.trace(error)
+            }
+            cb(null, response)
+        }))
+        .on('error',
+            function (error) {
+                console.log("Error occurred: " + error);
+            }
+        )
+        .on('finish',
+            function () {
+                console.log("Finished. ")
+            }
+        );
+}
 
 
 
