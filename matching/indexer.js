c3f87a4370f1443a4fa3c380023a3c8ccc5a6fb0
lfoppiano <luca@foppiano.org>
2018-10-30 09:05:11 +0100

    adding bulk indexing

--- matching/indexer.js
+++ matching/indexer.js
@@ -3,8 +3,11 @@
 let fs = require('fs');
 let es = require('event-stream');
 let lzma = require('lzma-native');
+let async = require('async');
 
 const elasticsearch = require('elasticsearch');
+
+const batchSize = 10000;
 
 //Create client
 let client = new elasticsearch.Client({
@@ -19,13 +22,13 @@ let client = new elasticsearch.Client({
 });
 
 // let analyserPath = "./resources/analyzer.json";
-let analyserPath = "/Users/lfoppiano/development/scienceminer/biblio-glutton/matching/resources/analyzer.json";
-let mappingPath = "./resources/crossref_mapping.json";
+let analyserPath = "resources/analyzer.json";
+let mappingPath = "resources/crossref_mapping.json";
 
 let input = '/Volumes/SEAGATE1TB/scienceminer/crossref/crossref-works.2018-09-05.json.xz';
 
 function createIndex(index) {
-    var analyserFile = fs.readFileSync(analyserPath, 'utf-8');
+    var analyserFile = fs.readFileSync(analyserPath, 'utf-8').toString();
     var mappingFile = fs.readFileSync(mappingPath, 'utf-8').toString();
 
     client.indices
@@ -89,7 +92,8 @@ init_cluster("crossref")
 
 
 function load_file(input) {
-    fs.createReadStream(input)
+    var start = new Date();
+    readStreamBao = fs.createReadStream(input)
         .pipe(lzma.createDecompressor())
         .pipe(es.split())
         .pipe(es.map(function (data, cb) {
@@ -104,34 +108,104 @@ function load_file(input) {
             delete obj.reference;
 
             cb(null, obj)
-        }))
-        .pipe(es.map(function (line, cb) {
+        }));
 
-            var response = undefined;
-            try {
+    async.series([
+        function (next) {
+            var i = 0;
+            var batch = [];
 
-                response = client.index({
-                    index: 'crossref',
-                    type: 'item',
-                    body: line
+            readStreamBao.on("data", function (doc) {
+                // console.log('indexing %s', doc.id);
+                batch.push({
+                    index: {
+                        "_index": 'crossref',
+                        "_type": 'work',
+                        "_id": doc.id.toString()
+                    }
                 });
 
-            } catch (error) {
-                console.trace(error)
-            }
-            cb(null, response)
-        }))
-        .on('error',
-            function (error) {
-                console.log("Error occurred: " + error);
-            }
-        )
-        .on('finish',
-            function () {
-                console.log("Finished. ")
-            }
-        );
-}
+                // var obj;
+                // if (doc.toObject) {
+                //     obj = doc.toObject();
+                // } else {
+                //     obj = doc;
+                // }
+                // obj = _.clone(obj);
 
+                // delete obj._id;
+                batch.push(doc);
+                i++;
+                if (i % batchSize == 0) {
+                    let intermediate_time = (new Date() - start) / 1000;
+                    console.log('Loaded %s records in %dms s (%dms record/s)', i, intermediate_time, batchSize / intermediate_time);
+                    client.bulk({
+                        refresh: "false", //we do refresh only at the end
+                        body: batch
+                    }, function (err, resp) {
+                        if (err) {
+                            next(err);
+                        } else if (resp.errors) {
+                            next(resp);
+                        }
+                    });
+                    batch = [];
+                }
+            });
+
+            // When the stream ends write the remaining records
+            readStreamBao.on("end", function () {
+                if (batch.length > 0) {
+                    console.log('Loaded %s records', batch.length);
+                    client().bulk({
+                        refresh: "wait_for", // we wait for this last batch before refreshing
+                        body: batch
+                    }, function (err, resp) {
+                        if (err) {
+                            console.log(err, 'Failed to build index');
+                            next(err);
+                        } else if (resp.errors) {
+                            console.log(resp.errors, 'Failed to build index');
+                            next(resp);
+                        } else {
+                            console.log('Completed crossref indexing.');
+                            next();
+                        }
+                    });
+                } else {
+                    next();
+                }
+
+                batch = [];
+            });
+        }]);
+
+    /*.pipe(es.map(function (line, cb) {
+
+        var response = undefined;
+        try {
+
+            response = client.index({
+                index: 'crossref',
+                type: 'item',
+                body: line
+            });
+
+        } catch (error) {
+            console.trace(error)
+        }
+        cb(null, response)
+    }))
+    .on('error',
+        function (error) {
+            console.log("Error occurred: " + error);
+        }
+    )
+    .on('finish',
+        function () {
+            console.log("Finished. ")
+        }
+    );*/
+}
 
 
