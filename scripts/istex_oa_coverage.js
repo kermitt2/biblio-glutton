3fb869a43d1403167588842fdce6bb28c7810d84
lopez <patrice.lopez@science-miner.com>
2018-12-06 13:25:08 +0100

    add istex OA coverage script

---
+++ scripts/istex_oa_coverage.js
@@ -0,0 +1,145 @@
+var http = require('http'),
+    fs = require('fs'),
+    zlib = require('zlib'),
+    readline = require('readline');
+
+// biblio-glutton server
+var host = "localhost"
+var port = "8080"
+var service = "/service/oa?doi="
+
+/**
+ *  usage: 
+ *
+ *  > npm install
+ *
+ *  > node istex_oa_coverage -dump ../data/istex/istexIds.all.gz
+ *
+ *  (dump file is gzipped)
+ */
+
+function processDump(options, callback) {
+    // read dump file line by line, get the DOI, check the OA 
+    // availability of the DOI via biblio-glutton
+    console.log(options.dump_path);
+
+    let rstream = readline.createInterface({
+        input: fs.createReadStream(options.dump_path).pipe(zlib.createGunzip())
+    });
+
+    //var rstream = fs.createReadStream(options.dump_path).pipe(zlib.createGunzip());
+    var total = 0; // total entries
+    var total_oa = 0; // total open access available
+    rstream.on('line', function(line) {
+        total++;
+        var json = JSON.parse(line);
+        
+        if (json.doi && json.doi.length > 0) {
+            //console.log(json.doi[0]);
+            var url = "http://" + options.glutton_host + ":" + options.glutton_port + options.service + json.doi[0]; 
+            //console.log(url);
+            var request = http.get(url, function(response) {
+                //console.log(response.statusCode);
+                if (response.statusCode == 200) {
+                    response.on("data", function(chunk) {
+                        console.log("[OA]: " + chunk);
+                        total_oa++;
+                    });
+                }
+            }).on('error', function(err) { // Handle errors
+                console.log('error calling glutton service:', json.doi[0]); 
+            });
+        }
+
+    });
+    rstream.on('finish', function (err) {
+        if (err) { 
+            console.log('error reading istex dump file', err)
+        } 
+
+        console.log("open access ratio: " + total_oa / total);
+
+        if (callback)
+            callback();
+    });
+}
+
+
+function callGlutton(doi, callback) {
+    var request = https.get(pdf_url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close(callGROBID(options, istexId, callback));  
+            // close() is async, call grobid after close completes.
+        });
+    }).on('error', function(err) { // Handle errors
+        fs.unlink(dest, function(err2) { if (err2) { 
+                return console.log('error removing downloaded PDF file'); 
+            } 
+        }); 
+        // delete the file async
+        if (callback) 
+            callback(err.message);
+    });
+
+    if (callback)
+        callback();
+}
+
+/**
+ * Init the main object with paths passed with the command line
+ */
+function init() {
+    var options = new Object();
+
+    // start with the config file
+    options.glutton_host = host;
+    options.glutton_port = port;
+    options.service = service;
+
+    // default service is full text processing
+    var attribute; // name of the passed parameter
+    // get the path to the PDF to be processed
+    for (var i = 2, len = process.argv.length; i < len; i++) {
+        if (process.argv[i-1] == "-dump") {
+            options.dump_path = process.argv[i];
+        } else if (!process.argv[i].startsWith("-")) {
+            options.action = process.argv[i];
+        } 
+    }
+
+    if (!options.dump_path) {
+        console.log("ISTEX ID dump path is not defines");
+        return;
+    }
+
+    // check the input path
+    fs.lstat(options.dump_path, (err, stats) => {
+        if (err)
+            console.log(err);
+        if (stats.isDirectory())
+            console.log("ISTEX ID dump path must be a file, not a directory");
+        if (!stats.isFile()) 
+            console.log("ISTEX ID dump path must be a valid file");
+    });
+
+    return options;
+}
+
+function end() {
+    var this_is_the_end = new Date() - start
+    console.info('Execution time: %dms', this_is_the_end)
+}
+
+var start;
+
+function main() {
+    var options = init();
+    start = new Date()
+    processDump(options, function(ratio) {
+        console.log("ratio:", ratio);
+        end();
+    });
+}
+
+main();
