32768441529124dc0b4132e368337499a9a79e49
lfoppiano <luca@foppiano.org>
2018-12-15 09:22:31 +0100

    first version async API for biblio lookup

--- lookup/src/main/java/com/scienceminer/lookup/web/resource/LookupController.java
+++ lookup/src/main/java/com/scienceminer/lookup/web/resource/LookupController.java
@@ -11,8 +11,13 @@ import com.scienceminer.lookup.storage.LookupEngine;
 import com.scienceminer.lookup.storage.StorageEnvFactory;
 
 import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.container.TimeoutHandler;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
@@ -37,6 +42,38 @@ public class LookupController {
         this.configuration = configuration;
         this.storageEnvFactory = storageEnvFactory;
         this.storage = new LookupEngine(storageEnvFactory);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/async")
+    public void asyncGetWithTimeout(
+            @QueryParam("doi") String doi,
+            @QueryParam("pmid") String pmid,
+            @QueryParam("pmc") String pmc,
+            @QueryParam("istexid") String istexid,
+            @QueryParam("firstAuthor") String firstAuthor,
+            @QueryParam("atitle") String atitle,
+            @QueryParam("postValidate") Boolean postValidate,
+            @QueryParam("jtitle") String jtitle,
+            @QueryParam("volume") String volume,
+            @QueryParam("firstPage") String firstPage,
+            @QueryParam("biblio") String biblio,
+            @Suspended final AsyncResponse asyncResponse) {
+        
+        asyncResponse.setTimeoutHandler(asyncResponse1 ->
+                asyncResponse1.resume(Response.status(Response.Status.SERVICE_UNAVAILABLE)
+                        .entity("Operation time out.")
+                        .build()
+                )
+        );
+        asyncResponse.setTimeout(60, TimeUnit.SECONDS);
+
+        new Thread(() -> {
+            String result = getByQuery(doi, pmid, pmc, istexid, firstAuthor, atitle,
+                    postValidate, jtitle, volume, firstPage, biblio);
+            asyncResponse.resume(result);
+        }).start();
     }
 
     @GET
